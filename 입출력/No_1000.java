f648e93bbdb1378e4bec50a441a0d8709019fd81
hyun1999 <dazzle1999@naver.com>
2023-01-05 20:12:09 +0900

    1000

---
+++ 입출력/No_1000.java
@@ -0,0 +1,13 @@
+package 입출력;
+
+import java.util.Scanner;
+
+public class No_1000 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        System.out.println(A+B);
+    }
+}
