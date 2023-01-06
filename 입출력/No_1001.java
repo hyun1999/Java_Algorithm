5e0c2ad932111babede7de8d53897ac0ef19cf4c
hyun1999 <dazzle1999@naver.com>
2023-01-06 15:34:47 +0900

    No_1001

---
+++ 입출력/No_1001.java
@@ -0,0 +1,12 @@
+package 입출력;
+
+import java.util.Scanner;
+
+public class No_1001 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        System.out.println(A-B);
+    }
+}
