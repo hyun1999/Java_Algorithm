4e7087dcef2f8ddebd37bbddfaa83625f6d83251
hyun1999 <dazzle1999@naver.com>
2023-01-07 17:36:58 +0900

    10926

---
+++ 입출력/no_10926.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class no_10926 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String Name = scan.nextLine();
+        System.out.println(Name+"??!");
+    }
+}
