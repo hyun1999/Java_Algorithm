28df0fcbed1b93cee47b9a7ed086ccea24ccf03b
hyun1999 <dazzle1999@naver.com>
2023-01-09 18:00:55 +0900

    9498

---
+++ 조건문/no_9498.java
@@ -0,0 +1,25 @@
+package 조건문;
+
+import java.util.Scanner;
+
+public class no_9498 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int Score = scan.nextInt();
+        if(Score >= 90){
+            System.out.println("A");
+        }
+        else if(Score >= 80){
+            System.out.println("B");
+        }
+        else if(Score >=70){
+            System.out.println("C");
+        }
+        else if(Score >= 60){
+            System.out.println("D");
+        }
+        else{
+            System.out.println("F");
+        }
+    }
+}
