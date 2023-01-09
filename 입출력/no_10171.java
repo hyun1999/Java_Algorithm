01f2bbc22b73bb8b99d79092c0a199277f5ba24a
hyun1999 <dazzle1999@naver.com>
2023-01-09 17:33:53 +0900

    10171

---
+++ 입출력/no_10171.java
@@ -0,0 +1,10 @@
+public class no_10171 {
+    public static void main(String[] args) {
+    
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");    
+
+    }
+}
