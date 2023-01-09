2373261c259418cdab7a56f7106733d5570dbb0d
hyun1999 <dazzle1999@naver.com>
2023-01-10 00:44:39 +0900

    10950

---
+++ 반복문/no_10950.java
@@ -0,0 +1,16 @@
+package 반복문;
+
+import java.util.Scanner;
+
+public class no_10950 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int T = scan.nextInt();
+
+        for(int i = 0;i<T;i++){
+            int A = scan.nextInt();
+            int B = scan.nextInt();
+            System.out.println(A+B);
+        }
+    }
+}
