91c362d16b15b7e5b7094c33d84a3e9322f5950a
hyun1999 <dazzle1999@naver.com>
2023-01-10 23:58:34 +0900

    10951

--- 반복문/no_10951.java
+++ 반복문/no_10951.java
@@ -2,20 +2,13 @@ package 반복문;
 
 import java.util.Scanner;
 
-public class no_10952 {
+public class no_10951 {
     public static void main(String[] args) {
-        
         Scanner scan = new Scanner(System.in);
-        while(true){
+        while(scan.hasNext()){
             int A = scan.nextInt();
             int B = scan.nextInt();
-
-            if(A==0 && B==0){
-                break;
-            }
-            else{
-                System.out.println(A+B);
-            }
+            System.out.println(A+B);
         }
     }
 }
