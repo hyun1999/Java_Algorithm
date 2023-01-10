ee0dcf1a16aa26f876f65ea6cbb0173ba37e2ef5
hyun1999 <dazzle1999@naver.com>
2023-01-10 23:45:59 +0900

    10952

---
+++ 반복문/no_10952.java
@@ -0,0 +1,23 @@
+package 반복문;
+
+import java.util.Scanner;
+
+public class no_10952 {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        while(true){
+            int A = scan.nextInt();
+            int B = scan.nextInt();
+
+            if(A==0 && B==0){
+                break;
+            }
+            else{
+                System.out.println(A+B);
+            }
+        }
+            
+        
+    }
+}
