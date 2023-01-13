ee1afc88e58e182fce3214a68df69848843d252e
hyun1999 <dazzle1999@naver.com>
2023-01-13 19:04:07 +0900

    10807

---
+++ 배열/no_10807.java
@@ -0,0 +1,22 @@
+package 배열;
+
+import java.util.Scanner;
+
+public class no_10807 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int T = scan.nextInt();
+        int[] arr = new int[T];
+        for(int i=0;i<T;i++){
+            arr[i] = scan.nextInt();
+        }
+        int count = 0;
+        int value = scan.nextInt();
+        for(int i=0;i<T;i++){
+            if(arr[i] == value){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
