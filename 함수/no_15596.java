9e5680fef8a3e5d3385d7dd1b166feef8d3877ba
hyun1999 <dazzle1999@naver.com>
2023-01-22 09:13:16 +0900

    15596

---
+++ 함수/no_15596.java
@@ -0,0 +1,12 @@
+package 함수;
+
+public class no_15596 {
+    long sum(int[] a){
+        long sum = 0;
+
+        for(int i = 0;i<a.length;i++){
+            sum += a[i];
+        }
+        return sum;
+    }
+}
