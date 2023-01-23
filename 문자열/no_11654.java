00a843be62dad544c4ae537c3f8b7e7462ea5c7b
hyun1999 <dazzle1999@naver.com>
2023-01-23 16:18:02 +0900

    11654

---
+++ 문자열/no_11654.java
@@ -0,0 +1,15 @@
+package 문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class no_11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        byte data = (byte)in.readLine().charAt(0);
+        System.out.println(data);
+        
+    }
+    
+}
